e50ba8072084a1a14bb3761210001014643305ed
Maycon Douglas <mdrs.maycon@gmail.com>
2024-02-11 15:21:49 -0300

    feat: data context com local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
-import { PrivateRoutes } from './pages/privateRoutes';
-import { LoginPage } from './pages/Login/LoginPage';
-import { HomePage } from './pages/Home/HomePage';
+import { PrivateRoutes } from '@/pages/privateRoutes';
+import { LoginPage } from '@/pages/Login/LoginPage';
+import { HomePage } from '@/pages/Home/HomePage';
 import { ThemeProvider } from "@/components/theme-provider"
+import { DataProvider } from '@/context/DataProvider';
 
 function App() {
-
   return (
-      <Routes>
-        <Route element={<PrivateRoutes />} >
-          <Route path="*" element={<Navigate to="/home" />} />
-          <Route path='/home' element={<HomePage />}>
+    <DataProvider>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <Routes>
+          <Route element={<PrivateRoutes />} >
+            <Route path="*" element={<Navigate to="/home" />} />
+            <Route path='/home' element={<HomePage />}>
+            </Route>
           </Route>
-        </Route>
-        <Route path='/login' element={<LoginPage />}></Route>
-      </Routes>
+          <Route path='/login' element={<LoginPage />}></Route>
+        </Routes>
+      </ThemeProvider>
+    </DataProvider>
   )
 }
 
