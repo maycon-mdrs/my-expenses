d9b966f7412559b847fc74135d4694bf5df89893
Maycon Douglas <mdrs.maycon@gmail.com>
2024-02-11 15:21:19 -0300

    Update HomePage.tsx

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,7 +1,33 @@
+import { DrawerData } from "@/components/dashboard/drawerData";
+import { Dashboard } from "@/components/dashboard/overview";
+import { ModeToggle } from "@/components/mode-toggle";
+import { MyNav } from "@/components/nav/MyNav";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataContext } from "@/context/DataProvider";
+import { useContext } from "react";
+
 export function HomePage() {
+    const data = useContext(DataContext);
+
+    async function handleAdd() {
+        
+    }
+
     return (
-        <div>
-        <h1>Home Page</h1>
-        </div>
+        <>  
+            <MyNav />
+            <DrawerData />
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+                <Card className="col-span-4">
+                    <CardHeader>
+                        <CardTitle>Overview</CardTitle>
+                    </CardHeader>
+                    <CardContent className="pl-2">
+                        <Dashboard />
+                    </CardContent>
+                </Card>
+            </div>
+        </>
     )
 }
